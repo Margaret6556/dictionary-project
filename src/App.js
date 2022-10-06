54bec6eae5c3333e2e139773852641a05f7a559c
cezarlofellem@gmail.com <cezarlofellem@gmail.com>
2022-10-05 22:46:13 -0400

    Major JS changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
 import "./App.css";
 import Dictionary from "./Dictionary";
+import logo from "./logo.png";
 
 export default function App() {
   return (
+
     <div className="App">
       <div className="container">
-        <header className="App-header">
-        <a href="https://www.margaretcezar.com" target="_blank" rel="noreferrer"><img src="/logo.png" className="logo" alt="Margaret logo" > </img> </a>
+        <header className="App-header">     
+        <img src={logo}  className="logo" alt="Margaret logo" > </img>
+          <p> Medical Terminologies Doesn't have to be so confusing!</p>
+          <h2> Welcome to your </h2>
+           <h1> Modern Medical Dictionary </h1>
         </header>
-        <main>
+
           <Dictionary />
-        </main>
+        
         <footer className="App-footer">
           <small>
             Coded by <a href="https://www.margaretcezar.com"> </a>{" "}
           </small>
         </footer>
       </div>
-    </div>
-  );
-}
+   </div>
+  
+   );
+  }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,45 +1,23 @@
 import React, { useState } from "react";
-import axios from "axios";
+import "./Dictionary.css";
 
 export default function Dictionary () {
-const [definition, setDefinition] = useState("");
-const [results, setResults] = useState (null);
+let [keyword, setKeyword] = useState("");
  
-function handleSubmit(event) {
+function search (event) {
  event.preventDefault();
+ alert (`Searching for ${keyword} definition`);
 }
 
- function handleResponse(response) {
-  setDefinition (response.data[0]);
- }
- 
- function search (event) {
-  event.preventDefault ();  
+ function handleKeywordChange (event) {
+ setKeyword (event.target.value); 
  }
 
- let apiKey="cd32822f9e64e59f7e2528e37e7549fa"
- let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/hello`
- axios.get(apiUrl).then(handleResponse);
-
- 
  return (
   <div className="Dictionary">
-  <div className="container">
- 
-  <header className="App-header">
- </header> 
-
- <main> 
-  <p> Medical Terminologies Doesn't have to be so confusing!<span role="img" aria-label="enpm run builmoji"> 🤯 </span></p>
-  <h2> Welcome to your </h2>
-  <h1> Modern Medical Dictionary </h1>
- </main>
-
-  <form onSubmit={handleSubmit}>
-  <input type="search /"> </input> onClick={setDefinition};
+    <form onSubmit={search}>
+  <input type="search "/> onChange={handleKeywordChange}; 
   </form>
-
-  </div>
   </div>
  );
 }
