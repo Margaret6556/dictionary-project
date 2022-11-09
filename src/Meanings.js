013ef0d314b098a98843a7cede3074422ae0539b
cezarlofellem@gmail.com <cezarlofellem@gmail.com>
2022-11-09 00:17:06 -0500

    Late push

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,25 +1,25 @@
 import React from "react";
 import Synonyms from "./Synonyms";
 
-export default function Meanings (props) {
-    console.log (props.meanings);
+export default function Meanings(props) {
+    console.log(props.meanings);
     return (
         <div className="Meanings">
             <h3>{props.meanings.PartOfSpeech}</h3>;
-            {props.meanings.definitions.map (function (definition, index) {
-            return (
-                <div key={index}>
-                    <p>
-                        <strong>Definition: </strong>
-                       
-                        {definition.definition}
-                        <br />
+            {props.meanings.definitions.map(function (definition, index) {
+                return (
+                    <div key={index}>
+                        <p>
+                            <strong>Definition: </strong>
 
-                        <Synonyms synonyms={synonyms}   
-                    </p>
-                </div>
-                     );
-})};
+                            {definition.definition}
+                            <br />
+
+                            <Synonyms synonyms={definition.synonyms} />
+                        </p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-export default function Synonyms {
+export default function Synonyms(props) {
+  if (props.synonyms) {
     return (
-        <div className="Synonyms">
-            <strong>Synonyms:</strong>
-            <em>{definition.synonyms}</em>
-                       
-        </div>
-    )
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>
+        })}
+      </ul>
+    );
+  } else {
+    return null;
+  }
 }
-
