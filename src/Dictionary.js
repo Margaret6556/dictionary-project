66c8a4b08058a2c9b230aaa52a4bc0802880e992
cezarlofellem@gmail.com <cezarlofellem@gmail.com>
2023-06-19 22:27:55 -0400

    update on Dictionary component style
    Map error keeps showing up

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,7 +14,6 @@ export default function Dictionary() {
     event.preventDefault();
 
 //temporary API source: https://www.shecodes.io/learn/apis/dictionary/
-
     let apiUrl = `https://www.dictionaryapi.com/api/v3/references/medical/json/${keyword}?key=39284b94tea8fb240cc5d3o3a355d5a0`;
     axios.get(apiUrl).then(handleResponse);
 }
@@ -26,8 +25,7 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <form className="mb-3" onSubmit={search}>
-        <input
-          className="input-search"
+        <input className="input"
           type="text"
           placeholder="Type your word"
           id="search "
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,12 +3,11 @@ import React from "react";
 export default function Phonetic (props) {
     console.log (props.phonetic);
     return (
-       <div> className="Phonetic"
+       <div className="Phonetic" >
         < a href={props.phonetics.audio} target="blank">
             Listen</a>
             <br />
             {props.phonetic.text}        
         </div>
-    )
-
+    );
 }
